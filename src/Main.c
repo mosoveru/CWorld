8a2f7e4ec0c80f31dce41237b50f678b7d81855e
Mosoveru <mosoveru@gmail.com>
2025-04-27 16:26:38 +0300

    feat: add number of symbols histogram

--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <io.h>
 #include <fcntl.h>
-#define maximumWordLength 17
+#define EnglishAlphabetLength 26
 
 main()
 {
@@ -9,40 +9,30 @@ main()
     _setmode(_fileno(stdin),  _O_U16TEXT);
     _setmode(_fileno(stderr), _O_U16TEXT);
 
-	int wordLength[maximumWordLength] = { 0 };
-	int isWord = 0, maxNumberOfWordLength = 0, tempLength = 0, c;
+	int numberOfLowercaseEnglishLetters[EnglishAlphabetLength] = { 0 }, c;
+	int numberOfUppercaseEnglishLetters[EnglishAlphabetLength] = { 0 };
 	while ((c = getwchar()) != WEOF) {
-		if (c != L' ' & c != L'\t' & c != L'\n') {
-			tempLength++;
-			isWord = 1;
+		if (c >= L'a' & c <= L'z') {
+			numberOfLowercaseEnglishLetters[c - L'a']++;
 		}
-		else if (tempLength < maximumWordLength && isWord) {
-			int currentLength = ++wordLength[tempLength];
-			if (currentLength > maxNumberOfWordLength)
-				maxNumberOfWordLength = currentLength;
-			tempLength = 0;
-			isWord = 0;
-		}
-		else {
-			tempLength = 0;
-			isWord = 0;
+		if (c >= L'A' & c <= L'Z') {
+			numberOfUppercaseEnglishLetters[c - L'A']++;
 		}
 	}
-	for (int i = 1; i < maximumWordLength; i++) {
-		wprintf(L"%4d", i);
-	}
-	wprintf(L"\n");
-	while (maxNumberOfWordLength > 0) {
-		for (int i = 1; i < maximumWordLength; i++) {
-			if (wordLength[i] > 0) {
-				wprintf(L"%4c", L'0');
-				wordLength[i]--;
-			}
-			else {
-				wprintf(L"%4c", L' ');
-			}
+	for (int i = 0; i < EnglishAlphabetLength; i++) {
+		wprintf(L"%c - ", L'a' + i);
+		while (numberOfLowercaseEnglishLetters[i] > 0) {
+			wprintf(L"0");
+			numberOfLowercaseEnglishLetters[i]--;
 		}
-		maxNumberOfWordLength--;
+		wprintf(L"\n");
+	}
+	for (int i = 0; i < EnglishAlphabetLength; i++) {
+		wprintf(L"%c - ", L'A' + i);
+		while (numberOfUppercaseEnglishLetters[i] > 0) {
+			wprintf(L"0");
+			numberOfUppercaseEnglishLetters[i]--;
+		}
 		wprintf(L"\n");
 	}
 }
