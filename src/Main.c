6bf408c43a867948090c0c4e5c77cdd6f0ef821d
Mosoveru <mosoveru@gmail.com>
2025-04-27 17:06:09 +0300

    feat: add header file and printLetters.c

--- src/Main.c
+++ src/Main.c
@@ -1,10 +1,8 @@
 ﻿#include <stdio.h>
 #include <io.h>
 #include <fcntl.h>
+#include "printLetters.h"
 #define EnglishAlphabetLength 26
-
-void printLowercaseEnglishLettersHistogram(int letters[]);
-void printUppercaseEnglishLettersHistogram(int letters[]);
 
 main()
 {
@@ -27,24 +25,3 @@ main()
 	return 0;
 }
 
-void printLowercaseEnglishLettersHistogram(int letters[]) {
-	for (int i = 0; i < EnglishAlphabetLength; i++) {
-		wprintf(L"%c - ", L'a' + i);
-		while (letters[i] > 0) {
-			wprintf(L"0");
-			letters[i]--;
-		}
-		wprintf(L"\n");
-	}
-}
-
-void printUppercaseEnglishLettersHistogram(int letters[]) {
-	for (int i = 0; i < EnglishAlphabetLength; i++) {
-		wprintf(L"%c - ", L'A' + i);
-		while (letters[i] > 0) {
-			wprintf(L"0");
-			letters[i]--;
-		}
-		wprintf(L"\n");
-	}
-}
---
+++ src/printLetters.h
@@ -0,0 +1,7 @@
+﻿#ifndef PRINT_LETTERS
+#define PRINT_LETTERS
+
+void printLowercaseEnglishLettersHistogram(int letters[]);
+void printUppercaseEnglishLettersHistogram(int letters[]);
+
+#endif
